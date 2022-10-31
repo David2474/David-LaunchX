9514779fd4dbb390a4c776e9ea83049dce67a3c3
JUAN DAVID GUTIERREZ GUTIERREZ <dg427938@gmail.com>
2022-10-30 21:05:57 -0600

    creando funciones para marcar tarea como hecha

--- script.js
+++ script.js
@@ -1,10 +1,53 @@
-const btn = document.querySelector("[btn-form]");
-let inp = document.querySelector('input');
+( ()=>{
 
-const createTask = () =>{
+const btn = document.querySelector("[btn-form]");
+const inp = document.querySelector('input');
+
+const createTask = (e) => {
+
     e.preventDefault();
-    console.log(inp.value)
+    const valor = inp.value;
+    const task = document.createElement("li");
+    task.classList.add("card");
+    const card = document.querySelector("[task-new]");
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checklist());
+    const titelTask = document.createElement("span");
+    titelTask.classList.add("task");
+    titelTask.innerText = valor;
+    taskContent.appendChild(titelTask);
+    const content = `
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+
+  //task.innerHTML = content;//
+    task.appendChild(taskContent)
+    card.appendChild(task);
+
+    task.appendChild(deletList())
+    
     inp.value = "";
 }
 
-btn.addEventListener("click", createTask())
+btn.addEventListener("click", createTask)
+
+const checklist = () => {
+    const i = document.createElement("i");
+    i.classList.add("far","fa-check-square","icon")
+    i.addEventListener("click", completeTask);
+    return i;
+}
+const deletList = () => {
+    const i = document.createElement("i");
+    i.classList.add("fas","fa-trash-alt","trashIcon","icon");
+    return i;
+}
+
+const completeTask = (event) =>{
+    const elemento = event.target;
+    elemento.classList.toggle("fas");
+    elemento.classList.toggle("completeIcon");
+    elemento.classList.toggle("far");
+    console.log(elemento)
+}
+
+})()
